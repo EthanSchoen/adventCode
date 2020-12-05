7010c7107b1fc9e9642d208e0c16e8025a3f4b71
Ethan Schoen <ethascho@gmail.com>
2020-12-05 09:40:54 -0500

    day 5

--- days/day05.py
+++ days/day05.py
@@ -1,3 +1,25 @@
 lines = open('days/day05_input.txt', "r").read().split('\n')
 print('~~~~~~~~~~~~~~~~~~~~~~~~~ Day 5 ~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+ids = []
+highest = -1
+lowest = 99999999999999
+for l in lines:
+  row = l[:7]
+  col = l[-3:]
+  row = row.replace('B','1')
+  row = row.replace('F','0')
+  col = col.replace('R','1')
+  col = col.replace('L','0')
+  seat_id = int(row, 2) * 8 + int(col, 2)
+  ids.append(seat_id)
+  highest = max(highest, seat_id)
+  lowest = min(lowest, seat_id)
+print('heighest seat id:', highest)
+
+all_ids = [i for i in range(lowest,highest+1)]
+for i in ids:
+  all_ids.remove(i)
+print('my seat:',all_ids)
+
 print()
