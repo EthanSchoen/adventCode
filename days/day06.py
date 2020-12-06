0907622d99d5e7eaf212568927f0f04b0e56c173
Ethan Schoen <ethascho@gmail.com>
2020-12-06 00:34:28 -0500

    day 6

--- days/day06.py
+++ days/day06.py
@@ -1,3 +1,30 @@
-lines = open('days/day06_input.txt', "r").read().split('\n')
+lines = open('days/day06_input.txt', "r").read().split('\n\n')
 print('~~~~~~~~~~~~~~~~~~~~~~~~~ Day 6 ~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+tally = {}
+for group in lines:
+  answers = []
+  for ans in group.split('\n'):
+    for c in ans:
+      if c not in answers:
+        answers.append(c)
+  for a in answers:
+    tally[a] = 1 if a not in tally else tally[a]+1
+print('part 1:', sum(tally.values()))
+
+tally = {}
+for group in lines:
+  answers = {}
+  group = group.split('\n')
+  for ans in group:
+    for c in ans:
+      answers[c] = 1 if c not in answers else answers[c] + 1
+  all_answered = []
+  for q in answers.keys():
+    if answers[q] == len(group):
+      all_answered.append(q)
+  for a in all_answered:
+    tally[a] = 1 if a not in tally else tally[a]+1
+print('part 2:', sum(tally.values()))
+
 print()
