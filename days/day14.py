e018541a55da2de1cda667ab5118f933ea6ebe2e
EthanSchoen <ethascho@gmail.com>
2020-12-14 13:05:09 -0500

    day 14

--- days/day14.py
+++ days/day14.py
@@ -2,24 +2,54 @@ lines = open('days/day14_input.txt', "r").read().split('\n')
 # lines = open('test.txt', "r").read().split('\n')
 print('~~~~~~~~~~~~~~~~~~~~~~~~~ Day 14 ~~~~~~~~~~~~~~~~~~~~~~~~~')
 
-mask = lines[0][:6:-1]
-print(mask)
+mask = ''
 mem = {}
-for ins in lines[1:]:
-  add = ins[4]
-  binary = str(bin(int(ins[9:])))[:1:-1]
-  for i in range(len(binary)):
+for ins in lines:
+  if ins[:4] == 'mask':
+    mask = ins[:6:-1]
+    continue
+  add = ins[ins.index('[')+1:ins.index(']')]
+  binary = str(bin(int(ins[ins.index('=')+2:])))[:1:-1]
+  for i in range(len(mask)):
+    if i >= len(binary):
+      binary = binary + ('0' if mask[i] == 'X' else mask[i])
+      continue
     if mask[i] == 'X':
       continue
     if mask[i] == '0':
-      print(binary)
       binary = binary[:i] +'0' + binary[i+1:]
-      print(binary)
     if mask[i] == '1':
-      # binary[i] = '1'
       binary = binary[:i] +'1' + binary[i+1:]
   binary = binary[::-1]
   mem[add] = int(binary, 2)
 print('Part 1:', sum(mem.values()))
+
+def place_in_mem(mem, add, val):
+  x = add.find('X')
+  if x == -1:
+    mem[str(int(add,2))] = val
+    return mem
+  return place_in_mem(mem, add[:x]+'0'+add[x+1:], val) | place_in_mem(mem, add[:x]+'1'+add[x+1:], val)
+print('\t(this is going to take a while)')
+mem = {}
+for ins in lines:
+  if ins[:4] == 'mask':
+    mask = ins[:6:-1]
+    continue
+  add = str(bin(int(ins[ins.index('[')+1:ins.index(']')])))[:1:-1]
+  value = int(ins[ins.index('=')+2:])
+  for i in range(len(mask)):
+    if i >= len(add):
+      add = add + mask[i:]
+      break
+    if mask[i] == '0':
+      continue
+    if mask[i] == '1':
+      add = add[:i] +'1' + add[i+1:]
+    if mask[i] == 'X':
+      add = add[:i] +'X' + add[i+1:]
+  add = add[::-1]
+  mem = place_in_mem(mem, add, value)
+print('Part 2:', sum(mem.values()))
 
 print()
