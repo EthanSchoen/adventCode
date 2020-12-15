ab3dcb59ead3a496329df8427bdc833036bf53a8
EthanSchoen <ethascho@gmail.com>
2020-12-15 11:18:43 -0500

    cleaned up day 15

--- days/day15.py
+++ days/day15.py
@@ -1,37 +1,21 @@
+import time
 print('~~~~~~~~~~~~~~~~~~~~~~~~~ Day 15 ~~~~~~~~~~~~~~~~~~~~~~~~~')
 
-numbers = [int(i) for i in [0,3,6]]
-turn = len(numbers) + 1
-new = 0
-while True:
-  last = numbers[len(numbers) - 1]
-  try:
-    new = numbers[::-1].index(last, numbers[::-1].index(last)+1) - numbers[::-1].index(last)
-  except:
-    new = 0
-  numbers.append(new)
-  if turn == 2020:
-    break
-  turn = turn + 1
+numbers = [0,3,6]
+record, new = {n: i for i, n in enumerate(numbers[:len(numbers)-1])}, numbers[len(numbers)-1]
+for turn in range(4, 2021):
+  last = new
+  new = 0 if not last in record else turn - 2 - record[last]
+  record[last] = turn - 2
 print('Part 1:', new)
 
-numbers = [int(i) for i in [12,1,16,3,11,0]]
-turn = len(numbers) + 1
-new = 0
-record = {}
-for i, n in enumerate(numbers[:len(numbers)-1]):
-  record[n] = i
-while True:
-  last = numbers[len(numbers) - 1]
-  if not last in record:
-    new = 0
-  else:
-    new = turn - 2 - record[last]
-  numbers.append(new)
+t = time.time()
+numbers = [12,1,16,3,11,0]
+record, new = {n: i for i, n in enumerate(numbers[:len(numbers)-1])}, numbers[len(numbers)-1]
+for turn in range(7, 30000001):
+  last = new
+  new = 0 if not last in record else turn - 2 - record[last]
   record[last] = turn - 2
-  if turn == 30000000:
-    break
-  turn = turn + 1
 print('Part 2:', new)
 
 print()
