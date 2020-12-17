a8205addf4cd313747430d0573415097a6c71e29
EthanSchoen <ethascho@gmail.com>
2020-12-17 13:41:25 -0500

    added timing to files

--- run.py
+++ run.py
@@ -1,14 +1,24 @@
 import sys
+import time
 
+t0 = time.time()
 if len(sys.argv) == 1:
   for a in range(1,26):
+    t = time.time()
     if a < 10:
       __import__('days.day0'+str(a))
     else:
       __import__('days.day'+str(a))
+    print('Time:',time.time()-t)
+    print()
 else:
+  t = time.time()
   for a in sys.argv[1:]:
+    t = time.time()
     if int(a) < 10:
       __import__('days.day0'+a)
     else:
       __import__('days.day'+a)
+    print('Time:',time.time()-t)
+    print()
+print('Total Time:',time.time()-t0)
