13fdcac10628d51a7f617f2521c0d3038a65e40a
EthanSchoen <ethascho@gmail.com>
2020-12-22 13:45:53 -0500

    day 18

--- days/day18.py
+++ days/day18.py
@@ -1,29 +1,21 @@
 import re
 
-# lines = open('days/day18_input.txt', "r").read().split('\n')
-lines = open('test.txt', "r").read().split('\n')
+lines = open('days/day18_input.txt', "r").read().split('\n')
+# lines = open('test.txt', "r").read().split('\n')
 print('~~~~~~~~~~~~~~~~~~~~~~~~~ Day 18 ~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 def eval(inp):
   stack = []
   for t in inp:
-    print(stack)
     if re.match(r'\d', t) is not None:
-      stack.insert(0,int(t))
-      # stack.append(int(t))
+      stack.append(int(t))
     else:
       if t == '+':
         a, b = stack.pop(), stack.pop()
         stack.append(a + b)
-      if t == '-':
-        a, b = stack.pop(), stack.pop()
-        stack.append(a - b)
       if t == '*':
         a, b = stack.pop(), stack.pop()
         stack.append(a * b)
-      if t == '/':
-        a, b = stack.pop(), stack.pop()
-        stack.append(a / b)
   return stack.pop()
 
 total = 0
@@ -35,7 +27,11 @@ for exp in lines:
       continue
     if re.match(r'\d', c) is not None:
       output.append(c)
-    elif re.match(r'[\+\-\*\/]', c) is not None or c == '(':
+    elif re.match(r'[\+\*]', c) is not None:
+      while opp and re.match(r'[\+\*]', opp[-1]) is not None:
+        output.append(opp.pop())
+      opp.append(c)
+    elif c == '(':
       opp.append(c)
     elif c == ')':
       top = opp.pop()
@@ -44,10 +40,36 @@ for exp in lines:
         top = opp.pop()
   while opp:
     output.append(opp.pop())
-  print(output)   
-  print(exp,' = ', eval(output))
-  print() 
-  # total = total + eval(output)
+  total = total + eval(output)
 print('Part 1:', total)
+
+total = 0
+for exp in lines:
+  output = []
+  opp = []
+  for c in exp:
+    if c == ' ':
+      continue
+    if re.match(r'\d', c) is not None:
+      output.append(c)
+    elif re.match(r'[\+\*]', c) is not None:
+      if c == '*':
+        while opp and re.match(r'[\+\*]', opp[-1]) is not None:
+          output.append(opp.pop())
+      if c == '+':
+        while opp and re.match(r'[\+\*]', opp[-1]) is not None and opp[-1] == '+':
+          output.append(opp.pop())
+      opp.append(c)
+    elif c == '(':
+      opp.append(c)
+    elif c == ')':
+      top = opp.pop()
+      while top != '(':
+        output.append(top)
+        top = opp.pop()
+  while opp:
+    output.append(opp.pop())
+  total = total + eval(output)
+print('Part 2:', total)
 
 print()
