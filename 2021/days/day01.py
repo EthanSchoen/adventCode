a5785be38f3903d3706c07b754ce98d540702943
EthanSchoen <ethascho@gmail.com>
2021-12-09 12:53:57 -0500

    added printing to template

--- 2021/days/day01.py
+++ 2021/days/day01.py
@@ -1,16 +1,23 @@
 
 input = open('days/day01_input.txt', "r").read().split('\n')
+numbers = list(map(int, input))
 
 print('~~~~~~~~~~~~~~~~~~~~~~~~~ Day 1 ~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 # Part 1
 def part1():
-  return
+  count = 0
+  last = numbers[0]
+  for n in numbers[1:]:
+    if n > last:
+      count += 1
+    last = n
+  print(count)
 
 # Part 2
 def part2():
   return
 
-part1()
-part2()
+print('part1:' + str(part1()))
+print('part2:' + str(part2()))
 print()
