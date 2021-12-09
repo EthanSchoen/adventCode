ce8940c6a1c9896b7431142c05612c7d5fa16eb1
Ethan Schoen <ethascho@gmail.com>
2021-12-09 17:51:34 -0500

    day 2

--- 2021/days/day02.py
+++ 2021/days/day02.py
@@ -5,11 +5,32 @@ print('~~~~~~~~~~~~~~~~~~~~~~~~~ Day 2 ~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 # Part 1
 def part1():
-  return
+  x, y = 0, 0
+  for line in input:
+    d, v = line.split(' ')
+    v = int(v)
+    if d == 'up':
+      y -= v
+    elif d == 'down':
+      y += v
+    elif d == 'forward':
+      x += v
+  return x * y
 
 # Part 2
 def part2():
-  return
+  a, x, y = 0, 0, 0
+  for line in input:
+    d, v = line.split(' ')
+    v = int(v)
+    if d == 'up':
+      a -= v
+    elif d == 'down':
+      a += v
+    elif d == 'forward':
+      x += v
+      y += a * v
+  return x * y
 
 print('part1: ' + str(part1()))
 print('part2: ' + str(part2()))
